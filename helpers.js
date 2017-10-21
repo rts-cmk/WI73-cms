c4e5da6f909d703a9a54d9c8de31f3abb613b191
Peter S. Lindenskov <psl@rts.dk>
2017-10-21 22:48:56 +0200

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -1,3 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+
+const mimetypes = {
+    '.html' : 'text/html',
+    '.css' : 'text/css',
+    '.js' : 'text/js',
+    '.png' : 'image/png'
+};
+
+exports.fileRespond = function(res, fileName){
+    console.log(fileName);
+    fs.readFile(fileName, function(err, fileContent){
+        if(err){
+            exports.respond(res, 'Fil ikke fundet', 404);
+            return;
+        }
+        var ext = path.extname(fileName);
+        var mime = mimetypes[ext];
+        res.writeHead(200, {'Content-type': mime})
+        res.end(fileContent);
+    });
+};
+
+
 exports.respond = function (res, besked, status = 200) {
     res.writeHead(status, { 'Content-type': 'application/json; charset=utf-8' });
     res.end(JSON.stringify(besked));
