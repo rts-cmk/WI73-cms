01c3d16ece05cdc195219659c161963ac9a60738
Peter S. Lindenskov <psl@rts.dk>
2017-10-30 21:50:59 +0100

    Update menuitems.js

--- endpointhandlers/menuitems.js
+++ endpointhandlers/menuitems.js
@@ -2,7 +2,7 @@ const helpers = require('./../helpers');
 const database = require('./../data/database');
 
 module.exports = {
-    'GET' :function(res){
+    'GET' :function(req, res){
         // helpers.respond(res, [{name : 'hjem'},{name : 'nyheder'},{name:'about'}]);
         var sql = "select * from menu order by position";
         database.select(res, sql, function(data){
