e711a69d58b7ff9cef822bff804d2fd821ff8dd1
Peter S. Lindenskov <psl@rts.dk>
2017-11-01 14:28:03 +0100

    Add files via upload

--- router.js
+++ router.js
@@ -1,42 +1,47 @@
-const url = require('url');
 const helpers = require('./helpers');
-
+const url = require('url');
 const routes = {
-    '/cat': require('./endpointhandlers/cat'),
-    '/dog': require('./endpointhandlers/dog'),
+    '/cat' : require('./endpointhandlers/cat'),
+    '/dog' : require('./endpointhandlers/dog'),
     '/menuitems' : require('./endpointhandlers/menuitems'),
     '/login' : require('./endpointhandlers/login'),
+    '/article' : require('./endpointhandlers/article')
+    // '/test' : require('./endpointhandlers/test')
+
 };
 
-module.exports = function (req, res) {
-    
+
+module.exports = function(req, res){
+    helpers.logger(req);
     var pathname = url.parse(req.url).pathname;
 
-    if(pathname === '/'){
-        helpers.fileRespond(res, 'public/index.html');
-        return;
+    if(pathname == '/'){
+      helpers.fileRespond(res, 'public/index.html'); 
+      return;   
     }
 
-    var regexFile = pathname.match(/^\/((css|js|img)\/)?\w+\.(html|css|js|png)$/);
+    var regexFile = pathname.match(/^\/(img\/|css\/|js\/)?[\w-]+\.(html|css|png|js|jpg)$/);
     if(regexFile){
         helpers.fileRespond(res, 'public' + regexFile[0]);
         return;
     }
-    
-    var action = routes[pathname];
-    if (action) {
-        var method = req.method;
-        var handler = action[method];
 
-        if (handler){
-            handler(req, res);
+    var method = req.method
+    var handler = routes[pathname]; // hent handleren (hvis vi har en)
+    if(handler){
+        // Hvis vi er her er der fundet en handler..
+        var action = handler[method]; // Hent metode (hvis vi har en der matcher)
+        if(action){
+            // Hvis vi er her er der fundet en metode-handler
+            action(req, res);    // Eksekver metodehandleren
         }
         else{
-            helpers.respond(res, `Metode ${req.method} ikke tilladt`, 404);
-            return;
+            // Hvis vi er her er der ikke fundet en metodehandler
+            helpers.respond(res, 'Metode ikke tilladt', 404);    
         }
-        return;
     }
-    // Hvis vi er her er der ikke fundet en route
-    helpers.respond(res, 'Route findes ikke', 404);
+    else{
+        // Hvis vi er her er der ikke fundet en handler
+        helpers.respond(res, 'Ressource findes ikke', 404);
+    }
 };
