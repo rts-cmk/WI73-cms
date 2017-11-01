9c3b3c5071b847574a26742d1e52e1f3b64fa8e6
Peter S. Lindenskov <psl@rts.dk>
2017-11-01 14:26:19 +0100

    Add files via upload

--- data/database.js
+++ data/database.js
@@ -1,21 +1,31 @@
 const mysql = require('mysql2');
 const helpers = require('./../helpers');
 
-const creds = {
-    host : 'localhost',
+const dbcreds = {
     user : 'wwwuser',
     password : 'wwwuser',
+    host : 'localhost',
     database : 'demo-cms'
 };
 
-const connection = mysql.createConnection(creds);
+const connection = mysql.createConnection(dbcreds);
 
 exports.select = function(res, sql, callback){
     connection.query(sql, function(err, data){
         if(err){
-            helpers.respond(res, {besked : 'Der opstod en fejl...'}, 404);
-            return
+            helpers.respond(res, {message : 'Databasefejl...', error : err});
+            return;
         }
         callback(data);
-    })
-}
+    });
+};
+
+exports.articleSelect = function(res, sql, values, callback){
+    connection.query(sql, values, function(err, data){
+        if(err){
+            helpers.respond(res, {message : 'Databasefejl...', error : err});
+            return;
+        }
+        callback(data);
+    });
+};
