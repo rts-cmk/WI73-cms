f747a222a87e7576a1be5d65b924ed9015fc1d62
Peter S. Lindenskov <psl@rts.dk>
2017-11-01 16:11:18 +0100

    Add files via upload

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,14 +1,15 @@
+
 fetch('/menuitems')
     .then(function(data){
         return data.json();
     })
-    .then(function(jsonData){
+    .then(function(jsonMenu){
         var menu = '';
-        jsonData.forEach(function(menuElement){
-            menu += '<span class="menuitem">' + menuElement.name + '</span>';
+        jsonMenu.forEach(function(menuItem){
+            menu += `<span class="menuitem" data-catid="${menuItem.id}">${menuItem.name}</span>`;
         });
         document.querySelector('#publicnavigationbar').innerHTML = menu;
     })
     .catch(function(err){
-        console.log(err);
-    })
+        console.log(err)
+    });
