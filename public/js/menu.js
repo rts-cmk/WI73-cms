4934d022e7cfe65226e726c2774cc63df43c598e
Peter S. Lindenskov <psl@rts.dk>
2017-11-03 11:44:04 +0100

    Update menu.js

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,15 +1,24 @@
-
-fetch('/menuitems')
-    .then(function(data){
-        return data.json();
-    })
-    .then(function(jsonMenu){
-        var menu = '';
-        jsonMenu.forEach(function(menuItem){
-            menu += `<span class="menuitem" data-catid="${menuItem.id}">${menuItem.name}</span>`;
+function menuUpdate() {
+    fetch('/menuitems', { credentials: 'include' })
+        .then(function (data) {
+            return data.json();
+        })
+        .then(function (jsonData) {
+            var menu = '';
+            jsonData.forEach(function (menuElement) {
+                menu += `<span class="menuitem" data-catid="${menuElement.id}" >${menuElement.name}</span>`;
+            });
+            document.querySelector('#publicnavigationbar').innerHTML = menu;
+        })
+        .then(function(){
+            document.querySelector('.menuitem').click();
+        })
+        .catch(function (err) {
+            console.log(err);
         });
-        document.querySelector('#publicnavigationbar').innerHTML = menu;
-    })
-    .catch(function(err){
-        console.log(err)
-    });
+}
+
+// Registrerer en eventhandler til 'DOMContentLoaded' eventen. 
+// Når dokumentet er loadet, kaldes 'menuupdate()' der sender en 
+// 'GET' request til routen '/menuitems'
+document.addEventListener('DOMContentLoaded', menuUpdate);
