df5319c5c7a9842c4f9b7d05891cf6cbbd55d335
Peter S. Lindenskov <psl@rts.dk>
2017-11-20 09:30:00 +0100

    Add files via upload

---
+++ admin/js/menuclick.js
@@ -0,0 +1,154 @@
+
+(function () {
+    document.addEventListener("click", menuclick, true);
+
+    function menuclick(e) {
+        // Forny timestamp på cookie
+
+        // var myCookie = 'id=';
+        // var cookieString = document.cookie;
+        // cookieString.split(';').forEach((c)=>{
+        //     if(c.match(/=/)){
+        //         if((c.split('=')[0]).trim()==='id'){
+        //             myCookie += c.split('=')[1].trim();
+        //         }
+        //     }
+        // })
+        // var expiretime = new Date(new Date().getTime() + 900000).toUTCString();
+        // myCookie += `; expires=${expiretime}`;
+        // document.cookie = myCookie;
+
+        var caller = e.target;
+        if (!caller.dataset.cmd) {
+            return;
+        }
+        if (document.querySelector('.itemActive')) {
+            document.querySelector('.itemActive').classList.toggle('itemActive');
+        }
+        caller.classList.toggle('itemActive');
+
+        switch (caller.dataset.cmd) {
+            case 'logout': // OK
+                logout();
+                break;
+            case 'categories': // OK 
+                categories();
+                break;
+            case 'catEdit': // OK
+                catEdit(caller);
+                break;
+            case 'catAdd':
+                catAdd(caller);
+                break;
+            case 'article':
+                break
+            case 'users':
+                alert(ds.cmd)
+                break;
+            default:
+                alert(caller);
+        }
+    }
+
+    function catEdit(caller) {
+        var formId = caller.dataset.id
+        var frm = document.querySelector(`#${formId}`);
+        var frmData = new FormData(frm);
+        fetch('/menuitems', {
+            credentials: 'include',
+            method: 'put',
+            body: frmData
+        })
+            .then(function (data) {
+                document.querySelector('div[data-cmd="categories"]').click();
+                // return data.json();
+            })
+        // .then(function (json) {
+        //     console.log(json);
+        // })
+    }
+
+
+    function logout() {
+        fetch('/logout', { credentials: 'include', method: 'delete' })
+            .then(function () {
+                document.querySelector("#title").innerHTML = 'Du loggede af...';
+                setTimeout(function () { location.href = "/"; }, 1000);
+            });
+    }
+
+    function categories() {
+        fetch('/menuitems', { method: 'get' })
+            .then(function (data) {
+                return data.json();
+            })
+            .then(function (jsonData) {
+                var content = `
+                            <div class="cat-container"><div class="cat-head">Rediger menu</div>
+                                <form>
+                                    <div class="cat-row">
+                                        <div class="cat-cell"><input readonly type="text" value="Navn"></div>
+                                        <div class="cat-cell"><input readonly type="text" value="Position"></div>
+                                    </div>
+                                </form>`;
+                jsonData.forEach(function (d) {
+                    content += `<form id="frm${d.id}">
+                                    <div class="cat-row">
+                                        <div class="cat-cell">
+                                            <input name="id" type="hidden" value="${d.id}">
+                                            <input name="catname" type="text" value="${d.name}">
+                                        </div>
+                                        <div class="cat-cell">
+                                            <input name="catpos" type="number" value="${d.position}">
+                                        </div>
+                                        <div class="cat-cell">
+                                            <img data-cmd="catEdit" data-id="frm${d.id}" class="iconImage clickable" src="img/plus-2x.png" title="Opdater">
+                                        </div>
+                                        <div class="cat-cell">
+                                            <img data-cmd="catDelete" data-id="frm${d.id}" class="iconImage clickable" src="img/minus-2x.png" title="Slet">
+                                        </div>
+                                    </div>
+                                </form>`;
+                });
+                content += `<br><hr>Eller tilføj<form id="frmCatAdd">
+                                <div class="cat-row">
+                                    <div class="cat-cell">
+                                        <input name="catname" type="text" placeholder="Menunavn">
+                                    </div>
+                                    <div class="cat-cell">
+                                        <input name="catpos" type="number" placeholder="position">
+                                    </div>
+                                    <div class="cat-cell">
+                                        <img data-cmd="catAdd"  class="iconImage clickable" src="img/plus-2x.png" title="Opdater">
+                                    </div>
+                                </div>
+                            <form>`;
+
+                // content += `<br><button type="button" data-cmd="catAdd" style="width:99%">Tilføj menupunkt</button>`
+                content += `</div>`;
+                document.querySelector('#content').innerHTML = content;
+            })
+            .catch(function (err) {
+                console.log(err);
+            })
+    }
+
+    function catAdd(caller){
+        var form = document.querySelector('#frmCatAdd');
+        var formData = new FormData(form);
+        fetch()
+        // console.log(formData.getAll('catname'));
+        // alert(caller.dataset.cmd);
+        
+    }
+
+    // Interval-functtion der holder øje med om session-cookien stadig eksisterer
+    setInterval(function () {
+        if (!document.cookie.length) {
+            alert("Du logges af nu...");
+            setTimeout(function () { location.href = "/"; }, 2000);
+        }
+    }, 1000 * 60 * 5)
+
+})();
+
