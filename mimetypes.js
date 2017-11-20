71d0c312064b90150a4002b187902886b0f4e12b
Peter S. Lindenskov <psl@rts.dk>
2017-11-20 10:43:14 +0100

    Add files via upload

---
+++ mimetypes.js
@@ -0,0 +1,7 @@
+module.exports = {
+    '.html' : {type : 'text/html; charset=utf-8', enc : 'utf-8'},
+    '.css'  : {type : 'text/css; charset=utf-8', enc : 'utf-8'},
+    '.js'   : {type : 'application/javascript; charset=utf-8', enc : 'utf-8'},
+    '.png'  : {type : 'image/png', enc : null},
+    '.jpg'  : {type : 'image/jpg', enc : null}
+};
