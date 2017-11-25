bc6698d2b9fde00dfd47667b27024800662b9321
Peter S. Lindenskov <psl@rts.dk>
2017-11-25 11:46:42 +0100

    Add files via upload

--- admin/js/menuclick.js
+++ admin/js/menuclick.js
@@ -30,11 +30,115 @@
             case 'article':
                 break
             case 'users':
-                alert(ds.cmd)
+                users(caller)
+                break;
+            case 'userAdd' :
+                userAdd(caller);
+                break;
+            case 'userEdit':
+                userEdit(caller);
+                break;
+            case 'userDelete' :
+                userDelete(caller);
                 break;
             default:
                 alert(caller);
         }
+    }
+
+    function users(){
+        fetch('/users', {credentials : 'include', method : 'get'})
+        .then(function(data){
+            return data.json();
+        })
+        .then(function(jsonData){
+            var content = `
+                <div class="tbl-container"><div class="tbl-head">Rediger brugere</div>
+                <form>
+                    <div class="tbl-row">
+                        <div class="tbl-cell"><input readonly type="text" value="Brugernavn"></div>
+                        <div class="tbl-cell"><input readonly type="text" value="Adgangskode"></div>
+                    </div>
+                </form>`;
+            jsonData.forEach(function (d) {
+                content += `<form id="frm${d.id}">
+                    <div class="tbl-row">
+                        <div class="tbl-cell">
+                            <input name="id" type="hidden" value="${d.id}">
+                            <input name="username" type="text" value="${d.username}">
+                            <input name="password" type="password" placeholder="Ny adgangskode">
+                        </div>
+                        <div class="tbl-cell">
+                            <img data-cmd="userEdit" data-id="frm${d.id}" class="iconImage clickable" src="img/Refresh.png" title="Opdater">
+                        </div>
+                        <div class="tbl-cell">
+                            <img data-cmd="userDelete" data-id="frm${d.id}" class="iconImage clickable" src="img/Trash.png" title="Slet">
+                        </div>
+                    </div>
+                </form>`;
+            });
+            content += `<br><hr>Eller tilføj<form id="frmUserAdd">
+                <div class="tbl-row">
+                    <div class="tbl-cell">
+                        <input name="username" type="text" placeholder="Brugernavn">
+                    </div>
+                    <div class="tbl-cell">
+                        <input name="password" type="text" placeholder="Adgangskode">
+                    </div>
+                    <div class="tbl-cell">
+                        <img data-cmd="userAdd"  class="iconImage clickable" src="img/plus-2x.png" title="Tilføj">
+                    </div>
+                </div>
+            <form>`;
+            content += `</div>`;
+            document.querySelector('#content').innerHTML = content;            
+        })
+    }
+
+    function userAdd(caller){
+        var form = document.querySelector('#frmUserAdd');
+        var formData = new FormData(form);
+        fetch('/users',{
+            method : 'post', 
+            credentials: 'include',
+            body : formData
+        })
+        .then(function (data) {
+            document.querySelector('div[data-cmd="users"]').click();
+        })        
+        .catch(function(err){
+            console.log(err);
+        });
+    }
+
+    function userEdit(caller){
+        var formId = caller.dataset.id;
+        var form = document.querySelector(`#${formId}`);
+        var formData = new FormData(form);
+        fetch('/users', {method : 'put', credentials : 'include', body : formData})
+        .then(function(){
+            document.querySelector('div[data-cmd="users"]').click();
+        })
+        .catch(function(err){
+            console.log(err);
+        })
+    }
+
+    function userDelete(caller){
+        var formId = caller.dataset.id;
+        var form = document.querySelector(`#${formId}`);
+        var formData = new FormData(form);
+        fetch('/users',{
+            method : 'delete', 
+            credentials: 'include',
+            body : formData
+        })
+        .then(function (data) {
+            document.querySelector('div[data-cmd="users"]').click();
+        })        
+        .catch(function(err){
+            console.log(err);
+        });
     }
 
     function catDelete(caller){
@@ -52,7 +156,6 @@
         .catch(function(err){
             console.log(err);
         });
-     
     }
 
     function catEdit(caller) {
@@ -85,41 +188,41 @@
             })
             .then(function (jsonData) {
                 var content = `
-                            <div class="cat-container"><div class="cat-head">Rediger menu</div>
+                            <div class="tbl-container"><div class="tbl-head">Rediger menu</div>
                                 <form>
-                                    <div class="cat-row">
-                                        <div class="cat-cell"><input readonly type="text" value="Navn"></div>
-                                        <div class="cat-cell"><input readonly type="text" value="Position"></div>
+                                    <div class="tbl-row">
+                                        <div class="tbl-cell"><input readonly type="text" value="Navn"></div>
+                                        <div class="tbl-cell"><input readonly type="text" value="Position"></div>
                                     </div>
                                 </form>`;
                 jsonData.forEach(function (d) {
                     content += `<form id="frm${d.id}">
-                                    <div class="cat-row">
-                                        <div class="cat-cell">
+                                    <div class="tbl-row">
+                                        <div class="tbl-cell">
                                             <input name="id" type="hidden" value="${d.id}">
                                             <input name="catname" type="text" value="${d.name}">
                                         </div>
-                                        <div class="cat-cell">
+                                        <div class="tbl-cell">
                                             <input name="catpos" type="number" value="${d.position}">
                                         </div>
-                                        <div class="cat-cell">
+                                        <div class="tbl-cell">
                                             <img data-cmd="catEdit" data-id="frm${d.id}" class="iconImage clickable" src="img/Refresh.png" title="Opdater">
                                         </div>
-                                        <div class="cat-cell">
+                                        <div class="tbl-cell">
                                             <img data-cmd="catDelete" data-id="frm${d.id}" class="iconImage clickable" src="img/Trash.png" title="Slet">
                                         </div>
                                     </div>
                                 </form>`;
                 });
                 content += `<br><hr>Eller tilføj<form id="frmCatAdd">
-                                <div class="cat-row">
-                                    <div class="cat-cell">
+                                <div class="tbl-row">
+                                    <div class="tbl-cell">
                                         <input name="catname" type="text" placeholder="Menunavn">
                                     </div>
-                                    <div class="cat-cell">
+                                    <div class="tbl-cell">
                                         <input name="catpos" type="number" placeholder="position">
                                     </div>
-                                    <div class="cat-cell">
+                                    <div class="tbl-cell">
                                         <img data-cmd="catAdd"  class="iconImage clickable" src="img/plus-2x.png" title="Opdater">
                                     </div>
                                 </div>
