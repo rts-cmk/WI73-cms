36e4da1cdd17d78163ccb9a0ab4735c2f0bdacb7
Peter S. Lindenskov <psl@rts.dk>
2017-11-25 11:43:07 +0100

    Update routedefinitions.js

--- routedefinitions.js
+++ routedefinitions.js
@@ -7,4 +7,5 @@ module.exports = {
     '/login'        : require('./endpointhandlers/login'),      // Login route, handler håndterer både GET og POST
     '/article'      : require('./endpointhandlers/article'),    // Article route handler håndterer GET, POST, 
     '/logout'       : require('./endpointhandlers/logout'),     // Logout route handler håndterer DELETE
+    '/users'        : require('./endpointhandlers/users')       // Users håndterer brugere (tilføj, fjern og update)
 }
