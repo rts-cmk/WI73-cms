e108cd050f5140c3e47211c2c14d550724f49612
Peter S. Lindenskov <psl@rts.dk>
2018-01-10 13:36:54 +0100

    Add files via upload

--- endpointhandlers/article.js
+++ endpointhandlers/article.js
@@ -5,10 +5,10 @@ const qs = require('querystring');
 
 module.exports = {
     'GET' : function(req, res){
-        // Denne funktion er public tilgængelig
         var cond, values;
         var query = url.parse(req.url).query
         var params = qs.parse(query);
+        // console.log(params);
         // Hvis params.catid, så hent artikler med cat_id
         // Hvis params.artid, så hent artiklen med artid
         if(params.catid){
@@ -19,78 +19,38 @@ module.exports = {
             cond = 'id';
             values = [params.artid];
         }
+        console.log(params);
         var sql = `select * from articles where ${cond} = ?`;
         database.articleselect(res, sql, values, function(data){
             helpers.respond(res, data);
-        });        
+        });
     },
     
     'POST' : function(req, res){
-        // Denne function er login beskyttet
-        var cookie = helpers.getCookies(req);
-        database.verifySession(res,cookie,function(veriFyData){
-            if(helpers.objEmpty(veriFyData)){
-                // Hvis vi er her er brugeren logget ikke ind....
-                helpers.redirect(res,'/login'); // ...derfor skal der sendes en rediect tilbage til browseren...
-                return; // ...og afsluttes med en return                
+        helpers.getFormData(req, res, function(formData){
+            if(helpers.objEmpty(formData)){
+                helpers.respond(res, {besked : "Der opstod en fejl"}, 500);
+                return;
             }
-            helpers.getFormData(req, res, function(formData){
-                if(helpers.objEmpty(formData)){
-                    helpers.respond(res, {besked : "Der opstod en fejl"}, 500);
-                    return;
-                }
-                var sql = 'insert into articles (category_id, title, content) values(?, ?, ?)';
-                var values = [formData.catId, formData.title, formData.article];
-                database.query(res, sql, values, function(data){
-                    helpers.respond(res, data);
-                });
+            var sql = 'insert into articles (category_id, title, content) values(?, ?, ?)';
+            var values = [formData.catId, formData.title, formData.article];
+            database.query(res, sql, values, function(data){
+                helpers.respond(res, data);
             });
         });
     },
     
-    'PUT' : function(req, res){
-        // Denne funktion er login beskyttet
-        var cookie = helpers.getCookies(req);
-        database.verifySession(res,cookie,function(veriFyData){
-            if(helpers.objEmpty(veriFyData)){
-                // Hvis vi er her er brugeren logget ikke ind....
-                helpers.redirect(res,'/login'); // ...derfor skal der sendes en rediect tilbage til browseren...
-                return; // ...og afsluttes med en return                
-            }        
-            helpers.getFormData(req, res, function(formData){
-                if(helpers.objEmpty(formData)){
-                    helpers.respond(res, {besked : "Der opstod en fejl"}, 500);
-                    return;
-                }
-                var sql = 'update articles set category_id = ?, title = ?, content = ? where id = ?';
-                var values = [formData.catId, formData.title, formData.article, formData.artid];
-                database.query(res, sql, values, function(data){
-                    helpers.respond(res, data);
-                });
-            });
-        });
-    },
-
     'DELETE' : function(req, res){
-        // Denne funktion er login beskyttet
-        var cookie = helpers.getCookies(req);
-        database.verifySession(res,cookie,function(veriFyData){
-            if(helpers.objEmpty(veriFyData)){
-                // Hvis vi er her er brugeren logget ikke ind....
-                helpers.redirect(res,'/login'); // ...derfor skal der sendes en rediect tilbage til browseren...
-                return; // ...og afsluttes med en return                
-            }        
-            helpers.getFormData(req, res, function(formData){
-                console.log(formData); 
-                if(helpers.objEmpty(formData)){
-                    helpers.respond(res, {besked : "Der opstod en fejl"}, 500);
-                    return;
-                }
-                var sql = 'delete from articles where id = ?';
-                var values = [formData.id];
-                database.query(res, sql, values, function(data){
-                    helpers.respond(res, data);
-                });
+        helpers.getFormData(req, res, function(formData){
+            console.log(formData); 
+            if(helpers.objEmpty(formData)){
+                helpers.respond(res, {besked : "Der opstod en fejl"}, 500);
+                return;
+            }
+            var sql = 'delete from articles where id = ?';
+            var values = [formData.id];
+            database.query(res, sql, values, function(data){
+                helpers.respond(res, data);
             });
         });
     }
--- endpointhandlers/users.js
+++ endpointhandlers/users.js
@@ -4,104 +4,42 @@ const database = require('./../data/database');
 module.exports = {
     // Henter alle brugere fra databasen og sender dem til browseren, men kun hvis brugeren er logget ind
     'GET': function (req, res) {
-        var cookie = helpers.getCookies(req);
-        database.verifySession(res,cookie,function(veriFyData){
-            if(helpers.objEmpty(veriFyData)){
-                // Hvis vi er her er brugeren logget ikke ind....
-                helpers.redirect(res,'/login'); // ...derfor skal der sendes en rediect tilbage til browseren...
-                return; // ...og afsluttes med en return
+        var sql = "SELECT id, username FROM users ORDER BY username";
+        var values = [1];
+        database.query(res, sql, values, function (data) {
+            if(helpers.objEmpty(data)){
+                helpers.respond(res, {besked : "Der opstod en fejl"}, 500);
+                return;
             }
-            // Hvis vi er her er brugeren logget ind, og vi kan fortsætte
-            var sql = "SELECT id, username FROM users ORDER BY username";
-            var values = [1];
-            database.query(res, sql, values, function (data) {
-                if(helpers.objEmpty(data)){
-                    helpers.respond(res, {besked : "Der opstod en fejl"}, 500);
-                    return;
-                }
-                helpers.respond(res, data);
-            });
+            helpers.respond(res, data);
         });
     },
 
     'POST' : function(req, res){
-        var cookie = helpers.getCookies(req);
-        database.verifySession(res,cookie,function(veriFyData){
-            if(helpers.objEmpty(veriFyData)){
-                // Hvis vi er her er brugeren logget ikke ind....
-                helpers.redirect(res,'/login'); // ...derfor skal der sendes en rediect tilbage til browseren...
-                return; // ...og afsluttes med en return
+        helpers.getFormData(req, res, function(formData){
+            if(helpers.objEmpty(formData)){
+                helpers.respond(res, {besked : "Der opstod en fejl"}, 500)
+                return;
             }
-            helpers.getFormData(req, res, function(formData){
-                if(helpers.objEmpty(formData)){
-                    helpers.respond(res, {besked : "Der opstod en fejl"}, 500)
-                    return;
-                }
-                if(formData.username[0].toString().length == 0 || formData.password[0].toString().length == 0){
-                    helpers.respond(res,{besked: 'Brugernavn og adgangskode skal begge være udfyldt'}, 500);
-                    return;
-                }
-                var sql = "insert into users (username, password) values(?, ?)"
-                var values = [formData.username, formData.password];
-                database.query(res, sql, values, function(data){
-                    helpers.respond(res, data);
-                })
-            });
+            var sql = "insert into users (username, password) values(?, ?)"
+            var values = [formData.username, formData.password];
+            database.query(res, sql, values, function(data){
+                helpers.respond(res, data);
+            })
         });
     },
 
     'PUT' : function(req, res){
-        // var cookie = helpers.getCookies(req);
-        // database.verifySession(res, cookie, )
         helpers.getFormData(req, res, function(formData){
             if(helpers.objEmpty(formData)){
                 helpers.respond(res, {besked : "Der opstod en fejl"}, 500);
                 return;
             }
-            // Vi skal huske at 'multiparty' modulet tager den indkommende forms elementer og organiserer dem i arrays
-            // Det betyder at selv om vi kun har et inputfelt der har 'name=username' vil havne i formData.username[0]
-
-            // Når vi skal validere input er der tre forskellige data der skal checkkes, username, password og id
-            // Mulige udfald, hvor 1 angiver om værdien findes og 0 angiver at den ikke findes og user = username, pwd = password og id = user_id
-            //      _________________________
-            //      |usr|pwd|id | opdatering |
-            //      |---|---|---|------------|
-            //      | 0 | 0 | 0 | ikke mulig |
-            //      | 0 | 0 | 1 | ikke mulig |
-            //      | 0 | 1 | 0 | ikke mulig |
-            //      | 0 | 1 | 1 | ikke mulig |
-            //      | 1 | 1 | 0 | ikke mulig |
-            //      | 1 | 0 | 1 | kun usr    |
-            //      | 1 | 1 | 0 | ikke mulig |
-            //      | 1 | 1 | 1 | usr og pwd |
-            //      --------------------------
-            // Som det ses af tabellen er det ikke muligt at opdatere hvis vi ikke har et id.
-            // Vi kræver også at username skal findes, men password behøves ikke nødvendigvis at findes.
-            if(formData.id[0].length < 1){
-                // Hvis vi er her det fordi vi ikke har noget id
-                helpers.respond(res, {besked : 'id må ikke være tomt'}, 400);
-                return;                
-            }
-            if(formData.username[0].length < 1){    // Check om der eksisterer et username
-                // Hvis vi er her er det fordi username ikke findes
-                helpers.respond(res, {besked : 'brugernavn må ikke være tomt'}, 400);
-                return;
-            }
-            // Hvis vi når hertil har vi både username og id
-            var sql = "update users set username = ?";
-
-            var values = [formData.username[0]];
-            if(formData.password[0].length > 1){
-                // Vi er her hvis vi har et password
-                sql += ', password = ?'
-                values.push(formData.password[0]);
-            }
-
-            sql += ' where id = ?'  // gør sql sætningen færdig
-            values.push(formData.id[0]);    // og tilføj det sidste element til values-arrayet
-
-            database.query(res, sql, values, function(data){    // Send sql sætningen til databasen...
-                helpers.respond(res, data);     // ...og send svaret til browseren.
+            var sql = "update users set username = ?, password = ? where id = ?";
+            var values = [formData.username, formData.password, formData.id];
+            console.log(values);
+            database.query(res, sql, values, function(data){
+                helpers.respond(res, data);
             })
         });
     },
@@ -109,16 +47,9 @@ module.exports = {
     'DELETE' : function(req, res){
         helpers.getFormData(req, res, function(formData){
             if(helpers.objEmpty(formData)){
-                // Vi eer her hvis vi ikke har nogle formData
-                helpers.respond(res, {besked : "Der opstod en fejl"}, 400);
+                helpers.respond(res, {besked : "Der opstod en fejl"}, 500);
                 return;
             }
-            if(formData.id[0].toString().length == 0){
-                // Hvis formData.id er tom er vi her
-                helpers.respond(res, {besked : "Der opstod en fejl"}, 400);
-                return;
-            }
-            // Vi er her hvis vi har de nødvendige data til rådighed til at gennemføre operationen
             // Jeg vil ikke tillade at 'admin' brugeren slettes. Jeg forudsætter at 'admin' har id = 1
             var sql = "delete from users where id = ? and id != 1";
             var values = [formData.id];
